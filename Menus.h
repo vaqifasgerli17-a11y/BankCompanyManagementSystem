d71ad0d7d35d2f5fdbd35abd42b9b409c79d7207
vaqifasgerli17-a11y <vaqifasgerli17@gmail.com>
2026-07-03 11:44:44 +0400

    Add Menus.h header file with menu function declarations

---
+++ Menus.h
@@ -0,0 +1,12 @@
+#ifndef MENUS_H
+#define MENUS_H
+
+#include "Bank.h"
+#include "Payment.h"
+#include "Employee.h"
+
+void bankMenu(Bank &bank);
+void paymentMenu(PaymentSystem &ps);
+void companyMenu(Company &company);
+
+#endif
