20af4a18c8e8dfeb648f6f43767c7c927b0927c4
vaqifasgerli17-a11y <vaqifasgerli17@gmail.com>
2026-09-14 21:37:18 +0400

    Remove comments from Payment.h

--- Payment.h
+++ Payment.h
@@ -4,9 +4,6 @@
 #include <string>
 #include <vector>
 
-// =====================================================================
-//  PAYMENT SYSTEM  (polimorfizm: Card / Cash / Crypto)
-// =====================================================================
 class Payment {
 protected:
     double amount;
