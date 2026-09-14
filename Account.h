b919e1297cbd75982bb1b96adacb0ec51bb3fc73
vaqifasgerli17-a11y <vaqifasgerli17@gmail.com>
2026-09-14 21:41:19 +0400

    Remove commented header from Account.h
    Removed commented header for account class.

--- Account.h
+++ Account.h
@@ -4,9 +4,6 @@
 #include <string>
 #include <fstream>
 
-// =====================================================================
-//  ACCOUNT  (Hesab + Kart + ATM birləşməsi)
-// =====================================================================
 class Account {
 private:
     int id;
